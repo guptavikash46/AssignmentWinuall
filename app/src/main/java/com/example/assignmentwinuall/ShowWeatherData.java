c546ce171c2600ea6d936bf54c30a18c525147e9
Vikas Gupta <guptavikash46@gmail.com>
2019-04-11 12:13:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/assignmentwinuall/ShowWeatherData.java
@@ -0,0 +1,13 @@
+package com.example.assignmentwinuall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowWeatherData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_weather_data);
+    }
+}
